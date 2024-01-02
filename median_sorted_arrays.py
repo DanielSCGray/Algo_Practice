f7e63c2d2e7363dd39dcc3077e797cf7a78d14ac
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2024-01-02 09:30:55 -0800

    median sorted arr solved 3x

--- median_sorted_arrays.py
+++ median_sorted_arrays.py
@@ -3,7 +3,7 @@
 # example, if given ([1,5,9], [1,2,3,4,5,6])​, return 4​. If the number of values is even, return the
 # average of the two middle values. if given ([1,5,9], [1,2,3,4,5])​, return 3.5​.
 
-#will make a merge helper function
+    #I see this as two parts: a merge function and a median finder. I will build both then use them for the three parts of the question
 def merge(nums1, nums2):
     total_len = len(nums1) + len(nums2)
     combined_array = []
@@ -22,15 +22,51 @@ def merge(nums1, nums2):
             combined_array.append(val)
     return combined_array
 
-print(merge([1,5,9], [1,2,3,4,5,6]))
+test = merge([1,5,9], [1,2,3,4,5,6])
+print(test)
+#[1, 1, 2, 3, 4, 5, 5, 6, 9]
+
+def median_finder(arr):
+    length = len(arr)
+    mid = length // 2
+    if length % 2 != 0:
+        return arr[mid]
+    else:
+        return ((arr[mid]+ arr[mid - 1])/ 2)
 
 
-def median_finder(nums1, nums2):
-    pass
+print(median_finder(test))
+#4
 
+#First:
 
+def median_of_sorted_arrays1(arr1, arr2):
+    combined = merge(arr1, arr2)
+    return median_finder(combined)
 
-
+print(median_of_sorted_arrays1([1,5,9], [1,2,3,4,5,6]))
+#4
 
 # Second:​ Expand your function to accept three arrays instead of two.
+def median_of_sorted_arrays2(arr1, arr2, arr3):
+    first_merge = merge(arr1, arr2)
+    combined = merge(first_merge, arr3)
+
+    return median_finder(combined)
+
+print(median_of_sorted_arrays2([1,5,9], [1,2,3,4,5,6], [1,2,10,13,15]))
+#4.5
+
 # Third:​ Rework your function to correctly handle an arbitrary number of arrays.
+
+def median_of_sorted_arrays3(arr1, arr2, *args):
+    combined = merge(arr1, arr2)
+    for arr in args:
+        temp_list = merge(combined, arr)
+        combined = temp_list
+    return median_finder(combined)
+
+print(median_of_sorted_arrays3([1,5,9], [1,2,3,4,5,6], [1,2,10,13,15]))
+#4.5
+print(median_of_sorted_arrays3([1,5,9], [1,2,3,4,5,6], [1,2,10,13,15], [2,4,7], [1,3,4,6,99]))
+#4.0
