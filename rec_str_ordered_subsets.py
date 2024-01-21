2f460329912f4614ed700bc47075c5cc109cd38c
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2024-01-21 15:19:56 -0800

    recursion againnn

---
+++ rec_str_ordered_subsets.py
@@ -0,0 +1,33 @@
+# String In-Order Subsets
+# Create strSubsets(str)​. Return array with all
+# possible in-order character subsets of str. The
+# result array itself need not be in a specific order.
+# Given "abc"​, return ["", "c", "b", "bc",
+# "a", "ac", "ab", "abc"]
+
+'''
+base is 1 char -> append and return
+for each letter in str do a recursive call on a str w/out that letter
+last letter special treatment to avoid index issues
+'''
+
+def str_subsets(string:str):
+    my_list = []
+    length = len(string)
+    my_list.append(string)
+    if length <= 1:
+        return my_list
+    last = length - 1
+    for i in range(length):
+        if i == last:
+            my_list.extend(str_subsets(string[:last]))
+        else:
+            txt = string[:i] + string[i + 1:]
+            my_list.extend(str_subsets(txt))
+    my_set = set(my_list)
+    my_list = list(my_set)
+    return my_list
+
+
+print(str_subsets("abc"))
+#['abc', 'c', 'ac', 'bc', 'ab', 'a', 'b']
