05988b0b17f5af9f3404b826da53685fcf898fa4
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2024-01-30 12:33:09 -0800

    set up

---
+++ 8_queens.py
@@ -0,0 +1,13 @@
+# Eight Queens
+# Build on previous solutions to write
+# eightQueens()​. Return all arrangements of
+# eight queens on an 8x8 chessboard, so that no
+# queen threatens any other. What is the best way
+# to return these results?
+# Second:​ write a helper function that displays the
+# results returned, using console.log().
+
+
+def eight_queens():
+    
+    return 
