bd9d37a29e4ada8c2645008a475460e99836ef8f
DanielSCGray <138812633+DanielSCGray@users.noreply.github.com>
2024-02-03 18:44:55 -0800

    longest word func

---
+++ longest_word.py
@@ -0,0 +1,23 @@
+# Longest Word
+# Create a function that, given a string of words,
+# returns the longest word. Example: given "Snap
+# crackle pop makes the world go
+# round!"​, return "crackle"​.
+# Bonus:​ handle punctuation
+
+def longest(string: str):
+    l = ""
+    tracker = 0 
+    for i in range(len(string)):
+        if string[i].isalpha() == False:
+            word = string[tracker:i]
+            if len(word) > len(l):
+                l = word
+            tracker = i +1
+    if len(string[tracker:]) > len(l):
+        return string[tracker:]
+    return l 
+
+
+print(longest("Snap crackle pop makes the world go round!"))
+#crackle
